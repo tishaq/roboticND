f8506d14c819eb8039574946c5aafd43f1a98fcf
tishaq <tishaq1@gmail.com>
2025-01-09 14:37:15 -0800

    Project 1: My world

---
+++ project_1_my_world/build/CMakeFiles/myworld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myworld.dir/script/myworld.cpp.o"
+  "libmyworld.pdb"
+  "libmyworld.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
